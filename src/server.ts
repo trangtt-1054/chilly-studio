55e3b86f5835c6916318e2409b512c77691404d9
Tran Thu Trang <tran.thu.trang-b@sun-asterisk.com>
2020-12-21 09:52:51 +0700

    create the first endpoint

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,16 @@ const server: Hapi.Server = Hapi.server({
 });
 
 export async function start(): Promise<Hapi.Server> {
+  //status endpoint
+  server.route({
+    method: "GET",
+    path: "/",
+    //handler: function gets called every time a request comes in, ResponseToolKit is a package of utilities, allow us to send response to user
+    handler: async (request: Hapi.Request, h: Hapi.ResponseToolkit) => {
+      return h.response({ up: true }).code(200);
+    },
+  });
+
   await server.start();
   console.log(`Server running on ${server.info.uri}`);
   return server;
